b6a48928dffc618c04417c8e0242097a2c180fab
InternetInutiPi09 <notan@email.com>
2022-01-31 14:47:26 +0000

    app and tests

--- app/app.py
+++ app/app.py
@@ -23,3 +23,31 @@ SPI1 = {
     'ce_pin':dio.DigitalInOut(board.D27),
     'csn':dio.DigitalInOut(board.D18),
     }
+
+def transmitter(address):
+    RF24.open_tx_pipe(address)
+
+def receiver(address):
+    RF24.open_rx_pipe(0, address)
+
+def main():
+    parser = argparse.ArgumentParser(description='NRF24L01+ test')
+    parser.add_argument('--src', dest='src', type=str, default='me', help='NRF24L01+\'s source address')
+    parser.add_argument('--dst', dest='dst', type=str, default='me', help='NRF24L01+\'s destination address')
+    parser.add_argument('--count', dest='cnt', type=int, default=10, help='Number of transmissions')
+    parser.add_argument('--size', dest='size', type=int, default=32, help='Packet size')
+    parser.add_argument('--txchannel', dest='txchannel', type=int, default=76, help='Tx channel', choices=range(0,125))
+    parser.add_argument('--rxchannel', dest='rxchannel', type=int, default=76, help='Rx channel', choices=range(0,125))
+
+    args = parser.parse_args()
+
+    value = input("Press 1 for transmitter or 0 for receiver: ")
+    if value == 1:
+        print("Transmitter")
+        transmitter(b"Transmit")
+    if value == 0:
+        print("Receiver")
+        receiver(b"Receive")
+
+if __name__ == "__main__":
+    main()
