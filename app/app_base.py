367eb3e1626c213a380fdd2f8580c0bac2565e7b
drawdehe <edward.sjoblom@gmail.com>
2022-03-04 13:43:16 +0000

    fragmentation

--- app/app_base.py
+++ app/app_base.py
@@ -23,71 +23,22 @@ def tx(count=0):
         packet = bytes_hex(packet)
 
         # Fragmentation
+        fragments = fragment(packet)
 
-        if packet[0:1].hex()[1] == '4': # IPv4
-            ihl = packet[1:2].hex()[1]
-            ihl = int(ihl)
-            header = packet[0:ihl*8]
-            print("Header: ", header)
-            payload = packet[ihl*8:]       
-            
-            fragments = []
-            count = 0
-            max_payload_size = 12 #in bytes
-            print("Payload before split: ", payload)
-            while sys.getsizeof(payload) > 0:
-                print("\nFragment no.", count+1)
-                if(sys.getsizeof(payload) <= max_payload_size*2): 
-                    # Add header w/ offset + no more fragments
-                    fragments.append(payload[:])
-                    payload = 0
-                    break
-                else:
-                    # Add header w/ or without offset + more fragments
-                    temp = list(header)
-
-                    # Set more fragments flag
-                    temp[12] = 50
-
-                    # Offset field
-
-                    offset = count * max_payload_size # payload is 12
-                    offset_hex = hex(offset)
-                    zoffset_hex = offset_hex[2:].zfill(3)
-
-                    # Convert new header to bytes, append fragmented payload
-
-                    new_header = bytes(temp)
-
-                    first_part_header = new_header[:13]
-                    last_part_header = new_header[16:]
-
-                    final_header = first_part_header + bytes(zoffset_hex, 'utf-8') + last_part_header
-                    print("Final header:  ", final_header)
-
-                    fragment = final_header + payload[:max_payload_size*2]
-                    fragments.append(fragment)
-                    print("Final fragment:", fragment)
-
-                    payload = payload[max_payload_size*2:]
-                count = count + 1
-
-        #for fragment in fragments:
-         #   radio_tx.write(fragment)
-
-        start_timer = time.monotonic_ns()
-        result = radio_tx.write(packet)
-        end_timer = time.monotonic_ns()
-        if not result:
-            print("Transmission failed or timed out")
-        else:
-            print(
-                "Transmission successful! Time to Transmit: "
-                "{} ms. Sent: {}".format(
-                    (end_timer - start_timer) / 1000000,
-                    packet
+        for f in fragments:
+            start_timer = time.monotonic_ns()
+            result = radio_tx.write(f)
+            end_timer = time.monotonic_ns()
+            if not result:
+                print("Transmission failed or timed out")
+            else:
+                print(
+                    "Transmission successful! Time to Transmit: "
+                    "{} ms. Sent: {}".format(
+                        (end_timer - start_timer) / 1000000,
+                        f
+                    )
                 )
-            )
     count += 1
 
 def rx(timeout=100):
@@ -108,7 +59,81 @@ def rx(timeout=100):
             )
             start_timer = time.monotonic()
 
-#def defragmentation(packets):
+def fragment(packet):
+    fragments = []
+
+    if packet[0:1].hex()[1] == '4': # IPv4
+        ihl = packet[1:2].hex()[1]
+        ihl = int(ihl)
+        header = packet[0:ihl*8]
+        print("Header: ", header)
+        payload = packet[ihl*8:]       
+        
+        nbr_fragments = 0
+        max_payload_size = 12 #in bytes
+        print("Payload before split: ", payload)
+        while len(payload) > 0:
+            print("\nFragment no.", nbr_fragments+1)
+            if(len(payload) <= max_payload_size*2): 
+                # Add header w/ offset + no more fragments
+
+                x = header[:13]
+                y = header[16:]
+
+                o = nbr_fragments * max_payload_size # payload is 12
+                o_hex = hex(o)
+                z_o_hex = o_hex[2:].zfill(3)
+
+                h = x + bytes(z_o_hex, 'utf-8') + y
+                f = h + payload[:]
+
+                fragments.append(f)
+                print("Fragment header:\t", h)
+                print("Fragment with payload:\t", f)
+                payload = 0
+                break
+            else:
+                # Add header w/ or without offset + more fragments
+                temp = list(header)
+
+                # Set more fragments flag
+                temp[12] = 50
+
+                # Offset field
+
+                offset = nbr_fragments * max_payload_size # payload is 12
+                offset_hex = hex(offset)
+                zoffset_hex = offset_hex[2:].zfill(3)
+
+                # Convert new header to bytes, append fragmented payload
+
+                new_header = bytes(temp)
+
+                first_part_header = new_header[:13]
+                last_part_header = new_header[16:]
+
+                final_header = first_part_header + bytes(zoffset_hex, 'utf-8') + last_part_header
+                print("Fragment header:\t", final_header)
+
+                fragment = final_header + payload[:max_payload_size*2]
+                fragments.append(fragment)
+                print("Fragment with payload:\t", fragment)
+
+                payload = payload[max_payload_size*2:]
+            nbr_fragments = nbr_fragments + 1
+    return fragments
+
+def defragment(fragment):
+    fragments = []
+    payload = 0
+    #while 
+    # Kolla more fragments
+    # Kolla identification
+    # Spara fragments i en array (?)
+    # Sortera array efter fragment offset
+    # Ta bort headers
+    # Sätt ihop payloads
+    return payload
 
 def get_image_from_the_internet():
     url = "https://cdn.pixabay.com/photo/2020/02/06/09/39/summer-4823612_960_720.jpg"
