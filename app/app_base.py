8491a1296e919ba0c7ea4cfcbec2921f63886e69
drawdehe <edward.sjoblom@gmail.com>
2022-03-14 21:38:36 +0000

    ping w queues works at most times

--- app/app_base.py
+++ app/app_base.py
@@ -4,7 +4,7 @@ import time
 import struct
 from RF24 import RF24, RF24_PA_LOW, RF24_2MBPS, RF24_250KBPS, RF24_CRC_8, RF24_CRC_DISABLED
 from tuntap import TunTap
-from multiprocessing import Process, Queue, RLock, Condition
+from multiprocessing import Process, Queue, RLock, Condition, Manager
 import wget
 import math
 from scapy.all import *
@@ -19,11 +19,11 @@ size = 2 ** 16 - 1
 def tx(queue, cond, count=0):
     radio_tx.stopListening()
     print("Running tx")
-    cond.acquire()
     while True:
+        cond.acquire()
         while queue.empty():
             print("Queue empty. Waiting...")
-            cond.wait(1)
+            cond.wait()
         print("transmitting")
         packet = queue.get(False)
         fragments = fragment(packet)
@@ -37,7 +37,7 @@ def tx(queue, cond, count=0):
             else:
                 print(
                     "Transmission successful! Time to Transmit: "
-                    "{} ms. Sent: {}. Size: {}".format(
+                    "{} ms. Sent: {}. Size: {}.".format(
                         (end_timer - start_timer) / 1000000,
                         f,
                         len(f)
@@ -49,10 +49,10 @@ def tx(queue, cond, count=0):
 
 def fragment(packet):
     # 4 byte header
-    nbr_of_frags = 2
-    frag_nbr = 2
+    nbr_of_frags = 4
+    frag_nbr = 4
     id_size = 0
-    payload_size = 16 - nbr_of_frags - frag_nbr - id_size
+    payload_size = 32 - nbr_of_frags - frag_nbr - id_size
     total_frags = math.ceil(len(packet)/payload_size)
     fragments = []
     for nbr in range(total_frags):
@@ -62,12 +62,13 @@ def fragment(packet):
         header = hex(nbr)[2:].zfill(4) + hex(total_frags)[2:].zfill(4)
         # header = hex(nbr)[2:].zfill(4) + hex(total_frags)[2:].zfill(4) + hex(id).zfill(2) #ifall vi kör id
         fragment_payload = packet[lower:upper]
-        final_fragment = (header + fragment_payload.hex())
-        final_fragment = final_fragment[::-1].zfill(32)[::-1] 
+        print(packet[lower:upper])
+        final_fragment = (header + fragment_payload)
+        # final_fragment = final_fragment[::-1].zfill(32)[::-1] 
         fragments.append(bytes(final_fragment, "utf-8"))
     return fragments
 
-def rx(condition, timeout=10000):
+def rx(cond, timeout=10000):
     radio_rx.startListening()
     start_timer = time.monotonic()
     fragments = []
@@ -90,9 +91,9 @@ def rx(condition, timeout=10000):
                     )
                     tun.write(codecs.decode(pkt,"hex")) # codecs.decode() gör om till samma typ av format som när man läser buffern i tx
                     fragments = []
-
             except UnicodeDecodeError:
                 print("Error, but ignore it")
+                print("Errored packet:", buffer)
             start_timer = time.monotonic()
     print("leaving rx")
 
@@ -117,8 +118,6 @@ def receive_from_internet(queue, cond, timeout=100):
 if __name__ == "__main__":
     radio_number = 0
     address = [b"1Node", b"2Node"]
-    queue = Queue()
-    cond = Condition()
 
     # Transmitter radio
     radio_tx = RF24(27, 10, 400000)
@@ -148,32 +147,37 @@ if __name__ == "__main__":
     radio_rx.setPayloadSize(32)
     radio_rx.setCRCLength(RF24_CRC_DISABLED)
 
-    print("rad 148")
     
+    with Manager() as manager:
+        queue = manager.Queue()
+        cond = manager.Condition()
     
-    tt = Process(target = tx, args=(queue, cond,))
-    rt = Process(target = rx, args=(cond,))
-    # it = Process(target = receive_from_internet, args=(queue, cond))
+        tt = Process(target = tx, args=(queue, cond,))
+        rt = Process(target = rx, args=(cond,))
+        it = Process(target = receive_from_internet, args=(queue, cond))
 
-    tt.start()
-    rt.start()
-    try:
-        print("Running receive_from_internet")
-        while True:
-            cond.acquire()
-            packet = tun.read(size)
-            print("Received from internet: ", packet.hex())
-            queue.put(packet.hex())
-            print("Queue size:", queue.qsize())
-            cond.notify_all()
-            cond.release()
-        print("leaving receive_from_internet")
-        # it.start()
-        # it.join()
-    except KeyboardInterrupt:
-        print("Keyboard Interrupt detected.")
-    tt.join()
-    rt.join()
+        tt.start()
+        rt.start()
+        it.start()
+        # try:
+        #     print("Running receive_from_internet")
+        #     while True:
+        #         cond.acquire()
+        #         packet = tun.read(size)
+        #         print("Received from internet: ", packet.hex())
+        #         queue.put(packet.hex())
+        #         print("Queue size:", queue.qsize())
+        #         cond.notify_all()
+        #         cond.release()
+        #     print("leaving receive_from_internet")
+        #     # it.start()
+        #     # it.join()
+        # except KeyboardInterrupt:
+        #     print("Keyboard Interrupt detected.")
+
+        tt.join()
+        rt.join()
+        it.join()
     
     radio_tx.powerDown()
     radio_rx.powerDown()
